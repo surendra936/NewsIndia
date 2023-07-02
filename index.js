aaa74d9fec152916995f357d2a55403fbc153360
Surendra <66784304+surendra936@users.noreply.github.com>
2023-07-02 15:15:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "b280fa2931454e6ea682cdad963dd858";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
